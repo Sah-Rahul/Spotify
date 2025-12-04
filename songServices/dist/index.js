7242d4669155b5187fb9ea72178242c9a2b654a4
Sah-Rahul <sahfamilyinfo@gmail.com>
2025-12-04 16:46:12 +0545

    redis setup

--- songServices/dist/index.js
+++ songServices/dist/index.js
@@ -3,11 +3,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.redisClient = void 0;
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const routes_1 = __importDefault(require("./routes"));
+const redis_1 = require("redis");
 dotenv_1.default.config();
+exports.redisClient = (0, redis_1.createClient)({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: "redis-10508.c240.us-east-1-3.ec2.cloud.redislabs.com",
+        port: 10508,
+    },
+});
+exports.redisClient
+    .connect()
+    .then(() => console.log("redis connected"))
+    .catch((err) => {
+    console.log("Redis error:", err.message);
+});
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 4000;
 app.get("/", (req, res) => {
@@ -16,7 +31,7 @@ app.get("/", (req, res) => {
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use('/api/v1', routes_1.default);
+app.use("/api/v1", routes_1.default);
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
--- songServices/src/index.ts
+++ songServices/src/index.ts
@@ -1,8 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import songRouter from "./routes"; 
+import songRouter from "./routes";
+import { createClient } from "redis";
+
 dotenv.config();
+
+export const redisClient = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: "redis-10508.c240.us-east-1-3.ec2.cloud.redislabs.com",
+    port: 10508,
+  },
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("redis connected"))
+  .catch((err) => {
+    console.log("Redis error:", err.message);
+  });
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -15,8 +32,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/v1", songRouter);
 
-app.use('/api/v1', songRouter)
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
